c08ccc955381452cd95264f7d4a73cd59b271d5a
Simo Nkosi <wilzmar.kwd@gmail.com>
2022-01-20 19:17:54 +0200

    Correct all rubocop offenses.

--- morse.rb
+++ morse.rb
@@ -1,60 +1,30 @@
 require_relative 'colors'
 
 def decode_char(character)
-    codes = {".-" => "A",
-        "-..." => "B",
-        "-.-." => "C",
-        "-.." => "D",
-        "." => "E",
-        "..-." => "F",
-        "--." => "G",
-        "...." => "H",
-        ".." => "I",
-        ".---" => "J",
-        "-.-" => "K",
-        ".-.." => "L",
-        "--" => "M",
-        "-." => "N",
-        "---" => "O",
-        ".--." => "P",
-        "--.-" => "Q",
-        ".-." => "R",
-        "..." => "S",
-        "-" => "T",
-        "..-" => "U",
-        "...-" => "V",
-        ".--" => "W",
-        "-..-" => "X",
-        "-.--" => "Y",
-        "--.." => "Z",
-        " " => " ",
-        ".----" => "1",
-        "..---" => "2",
-        "...--" => "3",
-        "....-" => "4",
-        "....." => "5",
-        "-...." => "6",
-        "--..." => "7",
-        "---.." => "8",
-        "----." => "9",
-        "-----" => "0",}
+  codes = { '.-' => 'A', '-...' => 'B', '-.-.' => 'C', '-..' => 'D', '.' => 'E', '..-.' => 'F',
+            '--.' => 'G', '....' => 'H', '..' => 'I', '.---' => 'J', '-.-' => 'K', '.-..' => 'L',
+            '--' => 'M', '-.' => 'N', '---' => 'O', '.--.' => 'P', '--.-' => 'Q', '.-.' => 'R',
+            '...' => 'S', '-' => 'T', '..-' => 'U', '...-' => 'V', '.--' => 'W', '-..-' => 'X',
+            '-.--' => 'Y', '--..' => 'Z', ' ' => ' ', '.----' => '1', '..---' => '2', '...--' => '3',
+            '....-' => '4', '.....' => '5', '-....' => '6', '--...' => '7', '---..' => '8', '----.' => '9',
+            '-----' => '0' }
 
-    if codes.has_key?(character)
-        return codes[character];
-    else
-        return "";
-    end
+  if codes.key?(character)
+    codes[character]
+  else
+    ''
+  end
 end
 
 def decode_word(word)
-    letters = word.split(/ /)
-    decoded_word = "";
+  letters = word.split(/ /)
+  decoded_word = ''
 
-    letters.each do |letter|
-        decoded_word += decode_char(letter)
-    end
+  letters.each do |letter|
+    decoded_word += decode_char(letter)
+  end
 
-    return decoded_word;  
+  decoded_word
 end
 
 # -------------Please write your code below this line -------------
@@ -62,16 +32,15 @@ end
 # -------------and above this line. Then delete the comments -------------
 
 def run_decoder()
-    puts "Enter Morse Code to decode:"
-    input = gets.chomp
+  puts 'Enter Morse Code to decode:'
+  input = gets.chomp
 
-    print "\n\n                     ...Decoding...".send(:red).send(:bold)
-    sleep 1
-    Gem.win_platform? ? (system "cls") : (system "clear")
-    
-    puts "\n\n\n\n\nYour decoded message is: #{decode(input).send(:green)}"
-    sleep 5
+  print "\n\n                     ...Decoding...".send(:red).send(:bold)
+  sleep 1
+  Gem.win_platform? ? (system 'cls') : (system 'clear')
 
+  puts "\n\n\n\n\nYour decoded message is: #{decode(input).send(:green)}"
+  sleep 5
 end
 
-run_decoder()
+run_decoder
